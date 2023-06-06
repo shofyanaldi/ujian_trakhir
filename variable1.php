ec4526642ebdfe702758b938aa85a487a48e48bf
shofyanaldi <124544009+shofyanaldi@users.noreply.github.com>
2023-06-06 22:56:30 +0700

    Add files via upload

---
+++ variable1.php
@@ -0,0 +1,30 @@
+<center>
+    <h1>
+    <?php
+        $nama = "cak tinggal";
+        $alamat = "jerebeng lor";
+        $hobi = "jalan jalan";
+        $mapel = "matematika";
+        $kelas = "xrpl 1";
+        $cita_cita = "nahkoda";
+        $ukuran_sepatu = "22";
+        $kuliah_jurusan = "......";
+    
+    echo "nama saya $nama";
+    echo "<br>";
+    echo "alamat $alamat";
+    echo "<br>";
+    echo "hobi $hobi";
+    echo "<br>";
+    echo "mapel $mapel";
+    echo "<br>";
+    echo "kelas $kelas";
+    echo "<br>";
+    echo "cita cita $cita_cita";
+    echo "<br>";
+    echo "ukuran sepatu $ukuran_sepatu";
+    echo "<br>";
+    echo "kuliah jurusan $kuliah_jurusan";
+    ?>
+    </h1>
+</center>
